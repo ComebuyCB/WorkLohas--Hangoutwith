fd95d34933e3b33df50f3ac52bc923d5cd055bf8
ComebuyCB <ben2000200066@yahoo.com.tw>
2022-02-18 19:06:18 +0800

    reg 補上傳

--- static/common/js/main.js
+++ static/common/js/main.js
@@ -28,3 +28,22 @@ SVGInject.setOptions({
     })
   },
 })
+
+
+$(document).on('click','.js-eye',function(){
+  let $input = $(this).closest('.js-input-eye').find('input')
+  let $eye = $(this).closest('.js-input-eye').find('.js-eye')
+  let inputType = $input.attr('type')
+  switch(inputType){
+    case 'password':
+      $eye.addClass('open')
+      $input.attr('type','text')
+    break
+    case 'text':
+      $eye.removeClass('open')
+      $input.attr('type','password')
+    break
+    default:
+    break
+  }
+})
